7e2e1b84f82eb24dd195cf2f3124bd03209cc4ef
Florian Klampfer <mail@qwtel.com>
2018-04-06 14:50:02 +0200

    update scripts

--- .scripts/version.js
+++ .scripts/version.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
-const { promisify } = require('util');
+const { promisify } = require("util");
 
-const { resolve } = require('path');
-const fs = require('fs');
+const { resolve } = require("path");
+const fs = require("fs");
 
-const vPrev = require('../assets/version.json').version;
-const vNext = require('../package.json').version;
+const vPrev = require("../assets/version.json").version;
+const vNext = require("../package.json").version;
 
 const readdir = promisify(fs.readdir);
 const rename = promisify(fs.rename);
@@ -15,26 +15,26 @@ const stat = promisify(fs.stat);
 const readFile = promisify(fs.readFile);
 const writeFile = promisify(fs.writeFile);
 
-const ENC = 'utf-8';
+const ENC = "utf-8";
 
 const FILES = [
-  resolve('./_data/authors.yml'),
-  resolve('./_includes/head/meta.html'),
-  resolve('./_includes/head/links.html'),
-  resolve('./_includes/head/styles.html'),
-  resolve('./_includes/header.txt'),
-  resolve('./_includes/body/scripts.html'),
-  resolve('./_includes/body/footer.html'),
-  resolve('./_layouts/compress.html'),
-  resolve('./_js/lib/version.js'),
-  resolve('./assets/version.json'),
+  resolve("./_data/authors.yml"),
+  resolve("./_includes/head/meta.html"),
+  resolve("./_includes/head/links.html"),
+  resolve("./_includes/head/styles.html"),
+  resolve("./_includes/header.txt"),
+  resolve("./_includes/body/scripts.html"),
+  resolve("./_includes/body/footer.html"),
+  resolve("./_layouts/compress.html"),
+  resolve("./_js/lib/version.js"),
+  resolve("./assets/version.json"),
 
-  resolve('./CHANGELOG.md'),
-  resolve('./download.md'),
-  resolve('./index.md'),
-  resolve('./README.md'),
-  resolve('./thank-you.md'),
-  resolve('./_includes/my-scripts.html'),
+  resolve("./CHANGELOG.md"),
+  resolve("./download.md"),
+  resolve("./index.md"),
+  resolve("./README.md"),
+  resolve("./thank-you.md"),
+  resolve("./_includes/my-scripts.html")
 ];
 
 // <https://stackoverflow.com/a/45130990/870615>
@@ -44,35 +44,35 @@ async function getFiles(dir) {
     subdirs.map(async subdir => {
       const res = resolve(dir, subdir);
       return (await stat(res)).isDirectory() ? getFiles(res) : res;
-    }),
+    })
   );
   return files.reduce((a, f) => a.concat(f), []);
 }
 
 (async function main() {
   try {
-    const prev = vPrev.replace(/\./g, '\\.');
-    const prevRegExp = new RegExp(prev, 'g');
+    const prev = vPrev.replace(/\./g, "\\.");
+    const prevRegExp = new RegExp(prev, "g");
 
     const [...args] = await Promise.all([
       FILES,
-      getFiles('./hyde/_posts'),
-      getFiles('./hydejack/_posts'),
-      getFiles('./_projects'),
-      getFiles('./docs'),
+      getFiles("./hyde/_posts"),
+      getFiles("./hydejack/_posts"),
+      getFiles("./_projects"),
+      getFiles("./docs")
     ]);
 
     const files = Array.prototype.concat.call(...args);
 
     const pFiles = Promise.all(
       files
-        .filter(([f]) => !f.startsWith('.'))
+        .filter(([f]) => !f.startsWith("."))
         .map(f => [f, readFile(f, ENC)])
         .map(async ([f, p]) => {
           const content = await p;
 
-          if (f.includes('CHANGELOG')) {
-            const pattern = new RegExp(`([^v])${prev}`, 'g');
+          if (f.includes("CHANGELOG")) {
+            const pattern = new RegExp(`([^v])${prev}`, "g");
             return [f, content.replace(pattern, `$1${vNext}`)];
           }
 
@@ -81,19 +81,20 @@ async function getFiles(dir) {
         .map(async p => {
           const [f, content] = await p;
           return writeFile(f, content, ENC);
-        }),
+        })
     );
 
-    const pJSCSS = Promise.all([
+    const pUnlink = Promise.all([
       unlink(resolve(`./assets/js/hydejack-${vPrev}.js`)),
-      unlink(resolve(`./assets/js/hydejack-${vPrev}.js.map`)),
-      rename(
-        resolve(`./assets/css/hydejack-${vPrev}.css`),
-        resolve(`./assets/css/hydejack-${vNext}.css`),
-      ),
-    ]);
+      unlink(resolve(`./assets/js/hydejack-${vPrev}.js.map`))
+    ]).catch(() => {});
 
-    await Promise.all([pFiles, pJSCSS]);
+    const pJSCSS = rename(
+      resolve(`./assets/css/hydejack-${vPrev}.css`),
+      resolve(`./assets/css/hydejack-${vNext}.css`)
+    );
+
+    await Promise.all([pUnlink, pFiles, pJSCSS]);
 
     await Promise.all([rename(resolve(`./docs/${vPrev}`), resolve(`./docs/${vNext}`))]);
 
