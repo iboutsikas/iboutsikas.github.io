164fe903c6ebc70134aec4e26e9d84984ba36631
Florian Klampfer <mail@qwtel.com>
2020-05-23 17:04:31 +0700

    Rename things

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -1,3 +1,3 @@
 ---
 ---
-{% include sw.js %}
+{% include service-worker.js %}
--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
 ---
 ---
-importScripts("{{ '/assets/js/sw.js' | relative_url }}?t={{ site.time | date_to_xmlschema }}");
+importScripts("{{ '/assets/js/service-worker.js' | relative_url }}?t={{ site.time | date_to_xmlschema }}");
