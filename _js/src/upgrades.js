a903dd5be86acaa3afba5f79759873e9b356ef80
Florian Klampfer <mail@qwtel.com>
2021-01-27 17:12:12 +0700

    Update mathjax

--- _js/src/upgrades.js
+++ _js/src/upgrades.js
@@ -213,11 +213,7 @@ const toggleClass = (element, ...cls) => {
 
   const mathJaxEl = document.getElementById('_MathJax');
   if (pushStateEl && mathJaxEl) {
-    const mathJax2To3 = ({
-      detail: {
-        replaceEls: [mainEl],
-      },
-    }) => {
+    const mathJax2To3 = ({ detail: { replaceEls: [mainEl] } }) => {
       mainEl.querySelectorAll('script[type="math/tex; mode=display"]').forEach((el) => {
         el.outerHTML = el.innerText.replace('% <![CDATA[', '\\[').replace('%]]>', '\\]');
       });
@@ -233,7 +229,7 @@ const toggleClass = (element, ...cls) => {
     await MathJax.typesetPromise();
 
     if (!window._noPushState) {
-      pushStateEl.addEventListener('ready', (e) => mathJax2To3(e));
+      pushStateEl.addEventListener('ready', mathJax2To3);
       fromEvent(pushStateEl, 'after')
         .pipe(concatMap(() => MathJax.typesetPromise()))
         .subscribe();
