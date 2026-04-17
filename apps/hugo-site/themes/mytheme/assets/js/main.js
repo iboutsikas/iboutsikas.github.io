045b586da25d08797c3fffeba121eaaa9db3e387
Ioannis Boutsikas <mgioanni@outlook.com>
2026-04-17 10:56:33 -0400

    Add proper fade in/out of content

--- apps/hugo-site/themes/mytheme/assets/js/main.js
+++ apps/hugo-site/themes/mytheme/assets/js/main.js
@@ -1,9 +1,11 @@
 import { initTheme } from './theme.js';
 import { initNav } from './nav.js';
 import { initCodeCopy } from './code-copy.js';
+import { initTransitions } from './transitions.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initTheme();
   initNav();
   initCodeCopy();
+  initTransitions({ initCodeCopy });
 });
